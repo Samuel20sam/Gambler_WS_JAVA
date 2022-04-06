37050c88941c9961edbcad6c111c8e2e2aaf6a10
Samuel_Elijah <samuel20sam@gmail.com>
2022-04-06 20:54:33 +0530

    Adding: Player making bet

--- src/com/bridgelabz/gambler/Gambler.java
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -3,13 +3,14 @@ package com.bridgelabz.gambler;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Gambler
-{
+public class Gambler {
     public static Scanner input = new Scanner(System.in);
     public static Random randNum = new Random();
 
-    public int stack = 100;
-    public int bet = 1;
+    public static int stack = 100;
+    public static int bet = 1;
+    public static int playCount = 0;
+    public static String toss;
 
     public static void main(String[] args) {
         System.out.println("""
@@ -17,28 +18,39 @@ public class Gambler
                 Enter your name""");
         String name = input.next();
         System.out.println("Welcome " + name);
-        bet();
+        play();
     }
 
-    public static void bet() {
+    public static void play() {
         System.out.println("make your bet - type 'H' for 'Head' or 'T' for 'Tail' ");
         String call = input.next();
-        System.out.println("Call is " +call);
-        if (call.equalsIgnoreCase("H")||call.equalsIgnoreCase("T")) {
+        System.out.println("Call is " + call);
+
+        if (call.equalsIgnoreCase("H") || call.equalsIgnoreCase("T")) {
             int random = randNum.nextInt(2);
             System.out.println("RandNum number is " + random);
-            
-            char toss;
+
             if (random == 0) {
-                toss = 'H';
+                toss = String.valueOf('H');
                 System.out.println("And it is Heads");
+
             } else {
-                toss = 'T';
+                toss = String.valueOf('T');
                 System.out.println("And it is Tails");
             }
         } else {
             System.out.println("Note: Only H or T ---- Make your bet - type 'H' for 'Head' or 'T' for 'Tail' ");
-            bet();
+            play();
+        }
+        if ((call.equalsIgnoreCase("H") && "H".equalsIgnoreCase(toss)) ||
+                (call.equalsIgnoreCase("T") && "T".equalsIgnoreCase(toss))) {
+            playCount++;
+            stack++;
+            System.out.println("After " + playCount + " round, now the stacks is " + stack);
+        } else {
+            playCount++;
+            stack--;
+            System.out.println("After " + playCount + "games, now the stacks is " + stack);
         }
     }
 }
